0fe2917c1785f354c3708bd854f6f9070663bfa6
dtron7th <dtron7th@gmail.com>
2020-05-18 16:32:17 -0400

    New Files

---
+++ Norin-UI.js
@@ -0,0 +1,9 @@
+function Options() {
+    var Click = document.getElementById ("content");
+
+    if (Click.style.display = "none") {
+        Click.style.display = "block";
+    } else{
+        Click.style.display = "none";
+    } 
+}
